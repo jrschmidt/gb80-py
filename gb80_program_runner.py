a1126aba5593d87b832782689b182120566b5969
jrschmidt <jrschmidt56@gmail.com>
2026-05-24 15:23:21 -0700

    add stubs for subsidiary program line runner methods, such as _run_remark(), _run_goto(), _run_if_then(), ...

--- gb80_program_runner.py
+++ gb80_program_runner.py
@@ -30,5 +30,74 @@ def _run_program(line_numbers: list[int], output_text: Callable) -> None:
 
 def execute_program_line(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
     op_type = line_object.get("op_type", "")
+    match op_type:
+        case "<remark>":             _run_remark(line_number, line_object, output_text)
+        case "<goto>":               _run_goto(line_number, line_object, output_text)
+        case "<if_then>":            _run_if_then(line_number, line_object, output_text)
+        case "<numeric_assignment>": _run_numeric_assignment(line_number, line_object, output_text)
+        case "<string_assignment>":  _run_string_assignment(line_number, line_object, output_text)
+        case "<numeric_input>":      _run_numeric_input(line_number, line_object, output_text)
+        case "<string_input>":       _run_string_input(line_number, line_object, output_text)
+        case "<numeric_print>":      _run_numeric_print(line_number, line_object, output_text)
+        case "<string_print>":       _run_string_print(line_number, line_object, output_text)
+        case "<end>":                _run_end(line_number, line_object, output_text)
+
+
+def _run_remark(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_goto(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_if_then(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_numeric_assignment(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_string_assignment(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_numeric_input(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_string_input(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_numeric_print(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_string_print(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
+    keyword = _BASIC_KEYWORDS.get(op_type, op_type)
+    output_text(f"LINE {line_number} {keyword}")
+
+
+def _run_end(line_number: int, line_object: BasicLine, output_text: Callable) -> None:
+    op_type = line_object.get("op_type", "")
     keyword = _BASIC_KEYWORDS.get(op_type, op_type)
     output_text(f"LINE {line_number} {keyword}")
