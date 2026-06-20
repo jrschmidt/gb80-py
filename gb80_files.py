3c97fc77c473578d510c3bb0a3e16e0d14ffafa5
jrschmidt <jrschmidt56@gmail.com>
2026-06-20 14:28:17 -0700

    SAVE command saves JSON version of BasicLine objects in the working directory

--- gb80_files.py
+++ gb80_files.py
@@ -1,8 +1,12 @@
 import json
 import re
+from pathlib import Path
 from typing import Callable
 
 from gb80_types import BasicLine
+from gb80_line_objects import get_line_numbers, get_line_object
+
+SAVE_DIR = Path("gb80_files")
 
 
 def program_to_json(program_lines: dict[int, BasicLine]) -> str:
@@ -14,4 +18,12 @@ def is_valid_gb80_filename(name: str) -> bool:
 
 
 def save_gb80_file(filename: str, output_text: Callable) -> None:
-    output_text("FILE SAVE IS NOT YET IMPLEMENTED")
+    SAVE_DIR.mkdir(exist_ok=True)
+    program_lines = {
+        n: get_line_object(n)
+        for n in get_line_numbers()
+        if get_line_object(n) is not None
+    }
+    path = SAVE_DIR / filename
+    path.write_text(program_to_json(program_lines))
+    output_text("PROGRAM SAVED")
